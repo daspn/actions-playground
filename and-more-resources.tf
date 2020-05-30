e73410c6f69663adf622de97bfa9b4364473143e
Daniel Augusto Paes Nunes <daniel.nunes@zoop.com.br>
2020-05-29 22:30:56 -0300

    Starting over

--- and-more-resources.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "c" {
-  bucket = "my-tf-test-bucket-999999"
-  acl    = "private"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
--- some-resources.tf
+++ some-resources.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket-112233"
+resource "aws_s3_bucket" "a" {
+  bucket = "my-tf-test-bucket-a"
   acl    = "private"
 
   tags = {
